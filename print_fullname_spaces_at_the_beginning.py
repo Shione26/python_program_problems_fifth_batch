c167ee3288cfc71dbb01932d801ee3763751d6b9
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-22 00:05:03 +0800

    ask for input

--- print_fullname_spaces_at_the_beginning.py
+++ print_fullname_spaces_at_the_beginning.py
@@ -1,4 +1,5 @@
 # Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 # ask for user input
+name = str(input("Input your fullname: "))
 # use python string method to remove extra spaces in the beginning then print
