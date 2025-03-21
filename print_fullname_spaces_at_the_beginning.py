20cc778e9b5af13e337cde4ba38fe6f746e11b65
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-22 00:03:45 +0800

    pseudocode

---
+++ print_fullname_spaces_at_the_beginning.py
@@ -0,0 +1,4 @@
+# Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+# ask for user input
+# use python string method to remove extra spaces in the beginning then print
