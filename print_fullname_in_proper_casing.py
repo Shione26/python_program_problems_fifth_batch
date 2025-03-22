60f2220b6939c25c1908d41d1ff47d7018bcaa7e
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-22 11:09:12 +0800

    ask for input

--- print_fullname_in_proper_casing.py
+++ print_fullname_in_proper_casing.py
@@ -1,5 +1,6 @@
 # Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
 
 # ask for input
+name = (input("Enter your fullname: "))
 # convert input into proper casing
 # print output
