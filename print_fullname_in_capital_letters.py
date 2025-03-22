991f8c2a82b67591e08087afe8d6d84a49060b0a
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-22 10:55:36 +0800

    pseudocode

---
+++ print_fullname_in_capital_letters.py
@@ -0,0 +1,5 @@
+# Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+# ask user for input
+# convert all input strings to capital letters
+# print output
