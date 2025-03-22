2b2fe24f7c3d850535756a0d917a8c2c46812638
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-22 11:47:15 +0800

    ask for input

--- count_number_of_characters.py
+++ count_number_of_characters.py
@@ -1,5 +1,6 @@
 # Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
 
 # ask user to input their fullname
+name = input("Enter your fullname: ")
 # count number of characters
 # print output
