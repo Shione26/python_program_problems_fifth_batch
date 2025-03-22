9f524dca5e1ddcfaa76e9c6b03fb77651d34b02b
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-22 11:21:25 +0800

    pseudocode

---
+++ count_number_of_words.py
@@ -0,0 +1,5 @@
+# Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+# ask user for input
+# count the number of words in the input
+# print the number of counts
