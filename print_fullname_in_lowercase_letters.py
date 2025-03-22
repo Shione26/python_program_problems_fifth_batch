44ea85b2f463009b12dbd3a2c870318c46a35a6e
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-22 11:02:31 +0800

    ask for input

--- print_fullname_in_lowercase_letters.py
+++ print_fullname_in_lowercase_letters.py
@@ -1,5 +1,6 @@
 # Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
 
 # ask for inputs
+name = input("Enter your fullname: ")
 # convert input to lowercase letters
 # print output
