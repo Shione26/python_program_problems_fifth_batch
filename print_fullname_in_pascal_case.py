d9f7b2d2677a786c5d0856d8be73a36f42e3d23a
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-22 11:53:16 +0800

    pseudocode

---
+++ print_fullname_in_pascal_case.py
@@ -0,0 +1,6 @@
+# Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+
+# ask the user for input in incorrect casing
+# convert the input to proper casing
+# remove space
+# print output
