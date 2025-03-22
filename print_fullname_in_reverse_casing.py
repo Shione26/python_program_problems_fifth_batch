d34c9b28d772b69e52d5d370d426571531f1dc0c
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-22 11:16:47 +0800

    pseudocode

---
+++ print_fullname_in_reverse_casing.py
@@ -0,0 +1,5 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+# ask user for input of fullname in incorrect casing
+# format each character to reverse casing
+# print output
