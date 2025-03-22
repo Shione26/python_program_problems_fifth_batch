26e3c01add3c3fcdc889a77fc0e3b17e98c5af59
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-22 10:48:29 +0800

    use zfill(6) to format number and fix AttributeError by removing int

--- print_the_number_in_six_digit_format.py
+++ print_the_number_in_six_digit_format.py
@@ -1,7 +1,7 @@
 # Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
 # ask the user to input number 0-1000
-number = int(input("Enter a number (0-1000): "))
+number = (input("Enter a number (0-1000): "))
 
 # format the input to 6 width characters
-
+print(number.zfill(6))
