0e84e2d4d1f44a98a6207329e6f4f750561f2d02
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-03-22 12:02:37 +0800

    ask for input

--- print_fullname_in_snake_case.py
+++ print_fullname_in_snake_case.py
@@ -1,6 +1,7 @@
 # Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 
 # ask the user to input fullname in incorrect casing
+name = input("Enter your fullname in incorrect casing: ")
 # convert the input to proper casing
 # replace spaces with underscore
 # print output
